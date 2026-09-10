75394911e4fe2df180eab4d24b68114f4af0c277
whskymtn <whskymtn@gmail.com>
2026-09-10 16:59:12 -0700

    Configure site URL for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://whskymtn.github.io',
+  base: '/portfolio', // Remove or set to '/' if your repository name is your-username.github.io
+});
